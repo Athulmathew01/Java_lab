aa33732a654fdf986454d51e453dc9307fc69e64
Athul Mathew Varughese <68552836+Athulmathew01@users.noreply.github.com>
2021-01-11 13:58:50 +0530

    Add files via upload

---
+++ java lab 2/areaandperimeter.java
@@ -0,0 +1,21 @@
+
+
+public class areaandperimeter {
+    public static void main(String args[])
+    {
+        System.out.println("---------AREA & PERIMETER OF THE TRIANGLE WITH SIDE 3,4 & 5 units--------");
+        triangle obj=new triangle();
+    
+
+    }
+    
+}
+class triangle
+{
+    triangle()
+    {
+        System.out.println("The area of the triangle is :"+(3*4*1/2));
+        System.out.println("The perimeter of the triangle is :"+(3+4+5));
+    }
+
+}
